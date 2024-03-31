1eacfc411dc7322767b0cada3ec06e40286f5c6d
Joseph C <josephalexcobian@gmail.com>
2024-03-31 00:38:06 -0700

    Download images instead of cloning repo.

--- stratagem-bank.js
+++ stratagem-bank.js
@@ -5,126 +5,126 @@ const data = {"stratagems" : [
         {
             "name" : "Machine Gun",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowRight"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Machine Gun.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Machine Gun.svg"
         },
         {
             "name" : "Anti-Material Rifle",
             "sequence" : [	"ArrowDown", "ArrowLeft", "ArrowRight", "ArrowUp", "ArrowDown"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Anti-Materiel Rifle.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Anti-Materiel Rifle.svg"
         },
         {
             "name" : "Stalwart",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowUp", "ArrowLeft"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Stalwart.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Stalwart.svg"
         },
         {
             "name" : "Expendable Anti-Tank",
             "sequence" : ["ArrowDown", "ArrowDown", "ArrowLeft", "ArrowUp", "ArrowRight"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Expendable Anti-Tank.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Expendable Anti-Tank.svg"
         },
         {
             "name" : "Recoiled Rifle",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowRight", "ArrowRight", "ArrowLeft"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Recoilless Rifle.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Recoilless Rifle.svg"
         },
         {
             "name" : "Flamethrower",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowUp", "ArrowDown", "ArrowUp"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Flamethrower.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Flamethrower.svg"
         },
         {
             "name" : "Autocannon",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowUp", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Autocannon.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Autocannon.svg"
     },
         {
             "name" : "Railgun",
             "sequence" : ["ArrowDown", "ArrowRight", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Railgun.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Railgun.svg"
     },
         {
             "name" : "Spear",
             "sequence" : ["ArrowDown", "ArrowDown", "ArrowUp", "ArrowDown", "ArrowDown"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Patriotic Administration Center/Spear.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Patriotic Administration Center/Spear.svg"
         },
         {
             "name" : "Eagle Strafing Run",
             "sequence" : ["ArrowUp", "ArrowRight", "ArrowRight"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Hangar/Eagle Strafing Run.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Strafing Run.svg"
         },
         {
             "name" : "Eagle Airstrike",
             "sequence" : ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowRight"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Hangar/Eagle Airstrike.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Airstrike.svg"
         },
         {
             "name" : "Eagle Cluster Bomb",
             "sequence" : ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowDown", "ArrowRight"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Hangar/Eagle Cluster Bomb.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Cluster Bomb.svg"
         },
         {
             "name" : "Eagle Napalm Airstrike",
             "sequence" : ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowUp"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Hangar/Eagle Napalm Airstrike.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Napalm Airstrike.svg"
         },
         {
             "name" : "Jump Pack",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowUp", "ArrowDown", "ArrowUp"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Hangar/Jump Pack.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Hangar/Jump Pack.svg"
         },
         {
             "name" : "Eagle Smoke Strike",
             "sequence" : ["ArrowUp", "ArrowRight", "ArrowUp", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Hangar/Eagle Smoke Strike.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle Smoke Strike.svg"
     },
         {
             "name" : "Eagle 110MM Rocket Pods",
             "sequence" : ["ArrowUp", "ArrowRight", "ArrowUp", "ArrowLeft"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Hangar/Eagle 110MM Rocket Pods.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle 110MM Rocket Pods.svg"
     },
 
         {
             "name" : "Eagle 500KG Bomb",
             "sequence" : ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowDown", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Hangar/Eagle 500KG Bomb.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Hangar/Eagle 500KG Bomb.svg"
     },
 
         {
             "name" : "Orbital Gatling Barrage",
             "sequence" : ["ArrowRight", "ArrowDown", "ArrowLeft", "ArrowUp", "ArrowUp"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Orbital Cannons/Orbital Airburst Strike.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Airburst Strike.svg"
     },
         {
             "name" : "Orbital Airburst Strike",
             "sequence" : ["ArrowRight", "ArrowRight", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Orbital Cannons/Orbital Airburst Strike.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Airburst Strike.svg"
     },
         {
             "name" : "Orbital 120MM HE Barrage",
             "sequence" : ["ArrowRight", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Orbital Cannons/Orbital 120MM HE Barrage.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital 120MM HE Barrage.svg"
     },
 
         {
             "name" : "Orbital Walking Barrage",
             "sequence" : ["ArrowRight", "ArrowDown", "ArrowUp", "ArrowUp", "ArrowLeft", "ArrowDown", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Orbital Cannons/Orbital Walking Barrage.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Walking Barrage.svg"
     },
 
         {
             "name" : "Orbital 380MM HE Barrage",
             "sequence" : ["ArrowRight", "ArrowRight", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Orbital Cannons/Orbital 380MM HE Barrage.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital 380MM HE Barrage.svg"
     },
         {
             "name" : "Orbital Lasers",
@@ -136,149 +136,149 @@ const data = {"stratagems" : [
             "name" : "Orbital Railcannon Strike",
             "sequence" : ["ArrowRight", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Orbital Cannons/Orbital Railcannon Strike.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Orbital Cannons/Orbital Railcannon Strike.svg"
     },
 
         {
             "name" : "Orbital Precision Strike",
             "sequence" : ["ArrowRight", "ArrowRight", "ArrowUp"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Bridge/Orbital Precision Strike.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Bridge/Orbital Precision Strike.svg"
     },
 
         {
             "name" : "Orbital Gas Strike",
             "sequence" : ["ArrowRight", "ArrowRight", "ArrowDown", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Bridge/Orbital Gas Strike.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Bridge/Orbital Gas Strike.svg"
     },
         {
             "name" : "Orbital EMS Strike",
             "sequence" : ["ArrowRight", "ArrowRight", "ArrowLeft", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Bridge/Orbital EMS Strike.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Bridge/Orbital EMS Strike.svg"
     },
         {
             "name" : "Orbital Smoke Strike",
             "sequence" : ["ArrowRight", "ArrowRight", "ArrowDown", "ArrowUp"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Bridge/Orbital Smoke Strike.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Bridge/Orbital Smoke Strike.svg"
     },
 
         {
             "name" : "HMG Emplacement",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight", "ArrowRight", "ArrowLeft"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Bridge/HMG Emplacement.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Bridge/HMG Emplacement.svg"
     },
 
         {
             "name" : "Shield Generation Relay",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowDown", "ArrowRight", "ArrowRight"],
-            "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Bridge/Shield Generator Relay.svg"
+            "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Bridge/Shield Generator Relay.svg"
         },
         {
             "name" : "Tesla Tower",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowRight", "ArrowUp", "ArrowLeft", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Bridge/Tesla Tower.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Bridge/Tesla Tower.svg"
     },
         {
             "name" : "Machine Gun Sentry",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowRight", "ArrowRight", "ArrowUp"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Robotics Workshop/Machine Gun Sentry.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Machine Gun Sentry.svg"
     },
         {
             "name" : "Gatling Sentry",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowRight", "ArrowLeft"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Robotics Workshop/Gatling Sentry.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Gatling Sentry.svg"
     },
         {
             "name" : "Mortar Sentry",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowRight", "ArrowRight", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Robotics Workshop/Mortar Sentry.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Mortar Sentry.svg"
     },
         {
             "name" : "Guard Dog",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowUp", "ArrowRight", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Robotics Workshop/Guard Dog.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Guard Dog.svg"
     },
         {
             "name" : "Autocannon Sentry",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowRight", "ArrowUp", "ArrowLeft", "ArrowUp"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Robotics Workshop/Autocannon Sentry.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Autocannon Sentry.svg"
     },
         {
             "name" : "Rocket Sentry",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowRight", "ArrowRight", "ArrowLeft"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Robotics Workshop/Rocket Sentry.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/Rocket Sentry.svg"
     },
         {
             "name" : "EMS Mortar Sentry",
             "sequence" : ["ArrowDown", "ArrowDown", "ArrowUp", "ArrowUp", "ArrowLeft"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Robotics Workshop/EMS Mortar Sentry.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/EMS Mortar Sentry.svg"
     },
         {
             "name" : "Anti-Personnel Minefield",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowUp", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Robotics Workshop/EMS Mortar Sentry.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Robotics Workshop/EMS Mortar Sentry.svg"
     },
 
         {
             "name" : "Supply Pack",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowUp", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Engineering Bay/Supply Pack.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Supply Pack.svg"
     },
         {
             "name" : "Grenade Launcher",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowUp", "ArrowLeft", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Engineering Bay/Grenade Launcher.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Grenade Launcher.svg"
     },
         {
             "name" : "Laser Cannon",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowDown", "ArrowUp", "ArrowLeft"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Engineering Bay/Laser Cannon.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Laser Cannon.svg"
     },
         {
             "name" : "Incendiary Mines",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowLeft", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Engineering Bay/Incendiary Mines.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Incendiary Mines.svg"
     },  {
             "name" : "Guard Dog Rover",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowUp", "ArrowRight", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Engineering Bay/Guard Dog Rover.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Guard Dog Rover.svg"
     },
 
           {
             "name" : "Ballistic Shield Backpack",
             "sequence" : ["ArrowDown", "ArrowLeft", "ArrowUp", "ArrowUp", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Engineering Bay/Ballistic Shield Backpack.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Ballistic Shield Backpack.svg"
     },
         {
             "name" : "Arc thrower",
             "sequence" : ["ArrowDown", "ArrowRight", "ArrowUp", "ArrowLeft", "ArrowDown"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Engineering Bay/Arc Thrower.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Arc Thrower.svg"
     },
         {
             "name" : "Shield Generator Pack",
             "sequence" : ["ArrowDown", "ArrowUp", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight"
         ],
-        "iconAddress" : "./images/Helldivers-2-Stratagems-icons-svg/Engineering Bay/Shield Generator Pack.svg"
+        "iconAddress" : "images/Helldivers-2-Stratagems-icons-svg-master/Engineering Bay/Shield Generator Pack.svg"
     }
     ]
 }
